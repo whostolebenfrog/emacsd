9a87c45c6255e913ff30f81fd9f5eb318eec1071
Ben Griffiths <ben@atomist.com>
2018-01-24 21:58:40 +0000

    Deps

---
+++ elpa/peg-20150707.2341/peg-autoloads.el
@@ -0,0 +1,15 @@
+;;; peg-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("peg.el") (23144 64511 925942 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; peg-autoloads.el ends here
