a8d171d8c6a4c0e4c0380e509dabc066241e166b
Ben Griffiths <ben@atomist.com>
2018-01-25 14:39:43 +0000

    Copy and paste working...

--- init.el
+++ init.el
@@ -117,6 +117,34 @@
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
+(require 'xclip)
+(xclip-mode 1)
+
+;; copy and paste
+(defun copy-to-clipboard ()
+  "Copies selection to x-clipboard."
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+	(message "Yanked region to x-clipboard!")
+	(call-interactively 'clipboard-kill-ring-save))
+    (if (region-active-p)
+	(progn
+	  (shell-command-on-region (region-beginning) (region-end) "pbcopy")
+	  (message "Yanked region to clipboard!")
+	  (deactivate-mark))
+      (message "No region active; can't yank to clipboard!"))))
+
+;; copy and paste
+(defun paste-from-clipboard ()
+  "Pastes from x-clipboard."
+  (interactive)
+  (if (display-graphic-p)
+      (progn
+	(clipboard-yank)
+	(message "graphics active"))
+    (insert (shell-command-to-string "pbpaste"))))
+
 ;; Bind things with evil goodness
 (evil-leader/set-key
   "f" 'cljr-clean-ns
@@ -131,4 +159,6 @@
   "w" 'cljr-rename-symbol
   "t" 'cider-test-run-test
   "T" 'cider-test-run-ns-tests
-  "H" 'clojure-cheatsheet)
+  "H" 'clojure-cheatsheet
+  "y" 'copy-to-clipboard
+  "p" 'paste-from-clipboard)
